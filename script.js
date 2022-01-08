bed5ad7931d89f1db2dfa44d5ab723e083450555
nebbieridzemariam <nebbieridze.mariam@gmail.com>
2022-01-09 00:44:55 +0400

    added tasks

---
+++ script.js
@@ -0,0 +1,72 @@
+// // #1
+
+
+// const currentDay = new Date().getDay()
+// switch (currentDay) {
+//     case 0:
+//     day = "Sunday";
+//     console.log ('Sunday');
+//     break;
+//   case 1:
+//     day = "Monday";
+//     console.log ('Monday');
+//     break;
+//   case 2:
+//      day = "Tuesday";
+//      console.log ('Tuesday');
+//     break;
+//   case 3:
+//     day = "Wednesday";
+//     console.log ('Wednesday');
+//     break;
+//   case 4:
+//     day = "Thursday";
+//     console.log ('Thursday');
+//     break;
+//   case 5:
+//     day = "Friday";
+//     console.log ('Friday');
+//     break;
+//   case 6:
+//     day = "Saturday";
+//     console.log ('Saturday');
+// }
+
+// // #2
+// {
+// let i = 0;
+// for ( i = 0; i < 101; i++) {
+//      console.log (i);
+// }
+// }
+
+
+
+// // #3
+// {
+// let i=0;
+// while (i < 50) {
+//     i++;
+//     console.log (i);
+// }
+// }
+
+
+
+// // #4
+
+// let i = 0;
+// do {
+//   i += 1;
+//   console.log(i);
+// } while (i < 150);
+
+
+
+// // #5
+// const myArray = [];
+// for (i = 0; i < 10000; i++) {
+//     pushArray = i * i
+//     myArray.push(pushArray);
+//     console.log(myArray);
+// }
